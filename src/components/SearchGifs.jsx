2c7239825d9949c8dea36f3070bb0f9482c174ab
Hilary Lewis <hilarylewis92@gmail.com>
2017-04-09 16:39:56 -0600

    On enter, consume searched for item gif api

--- src/components/SearchGifs.jsx
+++ src/components/SearchGifs.jsx
@@ -9,15 +9,10 @@ class SearchGifs extends Component {
     }
   }
 
-  handleChange(e) {
-    this.setState({
-      gif: e.target.value
-    })
-  }
-
   handleSubmit(e) {
+
     if(e.keyCode === 13) {
-      axios.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+      axios.get(`http://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=dc6zaTOxFJmzC `)
       .then(res => {
         console.log(res);
       })
@@ -30,7 +25,6 @@ class SearchGifs extends Component {
         <input className='search-gifs'
           type='text'
           placeholder='search for a gif'
-          onChange={(e) => this.handleChange(e)}
           onKeyUp={this.handleSubmit}/>
       </div>
     );
