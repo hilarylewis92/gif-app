3f312ff6ebabf2dac2edcf8db0ae91e3c177e3be
Hilary Lewis <hilarylewis92@gmail.com>
2017-04-09 18:42:51 -0600

    Add https

--- src/components/SearchGifs.jsx
+++ src/components/SearchGifs.jsx
@@ -13,7 +13,7 @@ class SearchGifs extends Component {
 
   handleSubmit(e) {
     if(e.keyCode === 13) {
-      axios.get(`http://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=dc6zaTOxFJmzC `)
+      axios.get(`https://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=dc6zaTOxFJmzC `)
       .then(res => {
         const gifs = res.data.data
         this.setState({
