dbcb24e3b167b03332b44aef930b23c00413f474
Hilary Lewis <hilarylewis92@gmail.com>
2017-04-12 08:55:47 -0600

    Add colors to styling

--- src/components/SearchGifs.jsx
+++ src/components/SearchGifs.jsx
@@ -15,7 +15,7 @@ class SearchGifs extends Component {
   handleSubmit(e) {
     if(e.keyCode === 13) {
       this.gsapAnimation()
-      axios.get(`https://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=dc6zaTOxFJmzC `)
+      axios.get(`https://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=dc6zaTOxFJmzC`)
       .then(res => {
         const gifs = res.data.data
         this.setState({
